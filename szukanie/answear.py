ae13e10406d6c4b8511b1b9645f94b4e4202accd
KarolinaH <paczeg@gmail.com>
2017-06-10 23:16:15 +0200

    Day 7
    dodanie nowych stronek do szukania

---
+++ szukanie/answear.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import bs4 as bs
+import requests
+import urllib.request
+import sys
+import re
+
+base_url = "https://www.picodi.com/pl/answear-com"
+soup  = BeautifulSoup(requests.get(base_url).content,"lxml")
+
+slowa = ["promocja","rabat",u"zniżka", "przecena", u"wyprzedaż", "%", "okazja", "outlet"]
+wyniki = {}
+
+for item in slowa:
+    result = soup.body.findAll(text=re.compile(item))
+    wyniki[item] = result
+
+
+orig_stdout = sys.stdout
+f = open("wynik_answear.txt","w")
+sys.stdout = f
+
+print(wyniki)
+
+
+sys.stdout = orig_stdout
+f.close()
--- szukanie/superpharm.py
+++ szukanie/superpharm.py
@@ -3,25 +3,25 @@ import bs4 as bs
 import requests
 import urllib.request
 import sys
+import re
 
 base_url = "https://www.superpharm.pl/super-pharm/gazetki-i-foldery/promocje.html"
 soup  = BeautifulSoup(requests.get(base_url).content,"lxml")
 
-result = [t.text for t in soup.select("div")
-          if "rabat" in t.text
-          or "sale" in t.text
-          or "promocja" in t.text
-          or "%" in t.text
-          or "zniżka" in t.text
-          or "obniżka" in t.text
-          ]
+slowa = ["promocja","rabat",u"zniżka", "przecena", u"wyprzedaż", "%", "okazja"]
+wyniki = {}
+
+for item in slowa:
+    result = soup.body.findAll(text=re.compile(item))
+    wyniki[item] = result
+
 
 orig_stdout = sys.stdout
 f = open("wynik_superpharm.txt","w")
 sys.stdout = f
 
-for element in result:
-    print(element)
+print(wyniki)
+
 
 sys.stdout = orig_stdout
 f.close()
